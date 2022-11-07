08a41422e21786a3217ba202257b5283b5de2d3a
JhonasML <jhonas.mutton@mercadolivre.com>
2022-11-07 19:26:25 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true, // Indica que vai usar o compilador do Styled Components para ajudar.
+    },
+  }
+  
+  module.exports = nextConfig
